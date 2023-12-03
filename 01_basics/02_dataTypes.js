21d955dd43ae111d2d15585465d76eca35abfaff
kunal <97289128+Kunalmarathe@users.noreply.github.com>
2023-12-03 06:11:38 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,34 @@
+
+"use strict"; // treat all js code as newer version , here all code treat as new standards , now no need to use this word because
+              // now all code run in strict mode
+
+// alert (3 + 3) // we are using nodejs, not browser
+
+// engine of a js is hidden in browser, if we use above alert() for arithmatic in developer console it gives ans because it is in document
+// it's not good practice to use console.log() twice on same line for better redability.
+// for better indentation use Prettier - code formatter extensions
+console.log(3 + 
+    3) // Not good pratice write it like console.log(3 + 3), code redability should be high
+
+// for documentation use tc39.es it's main, for easy purpose use mdn documentation
+
+console.log("Kunal");
+
+let name = "kunal"
+let age = 24
+let values = false
+
+// Primitive data types below:
+// number => 2 to the powerOf 54
+// bigint is the number which use for biggest digits like stock market
+
+// null = standalone value for ex below
+
+let plate = null // now the plate is not undefine but it's empty here
+
+// symbol => unique , here it use to represent the uniqueness
+
+//object
+
+console.log(typeof null)// otput is object , ### asked in interviwe
+console.log(typeof undefined) 
